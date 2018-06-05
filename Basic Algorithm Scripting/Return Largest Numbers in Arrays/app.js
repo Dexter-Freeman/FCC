7b7d29f9350828320478f87cb6c15ee794c7c466
Dexter Freeman <vse.pisma.tut@gmail.com>
2018-06-05 16:57:54 +0300

    Confirm the Ending  Return Largest Numbers in Arrays

---
+++ Basic Algorithm Scripting/Return Largest Numbers in Arrays/app.js
@@ -0,0 +1,10 @@
+function largestOfFour(arr) {
+  // You can do this!
+  let answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    answer.push(Math.max(...arr[i]));
+  }
+  return answer;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
