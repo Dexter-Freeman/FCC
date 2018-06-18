28c415589dd70e6150c7ccf48d626c1e997a6e88
Dexter <vse.pisma.tut@gmail.com>
2018-06-18 13:46:34 +0300

    Spinal Tap Case / Pig Latin

---
+++ Intermediate Algorithm Scripting/#5 Spinal Tap Case/app.js
@@ -0,0 +1,23 @@
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+
+  // I do not like this decision. But it works for these tests
+  const strPrep = (str) => {
+    let answer = str[0];
+    for (let i = 1; i < str.length; i++) {
+      if (str[i] === " " | str[i] === "_" | str[i] === "-") {
+        answer += "-";
+      } else if (str[i] !== " " && str[i] === str[i].toUpperCase() && str[i - 1] !== str[i - 1].toUpperCase()) {
+        answer += `-${str[i]}`;
+      } else {
+        answer += str[i];
+      }
+    }
+    console.info(`answer - ${answer}`);
+    return answer;
+  };
+  return strPrep(str).toLowerCase();
+}
+
+document.write(spinalCase('This Is Spinal Tap'));
