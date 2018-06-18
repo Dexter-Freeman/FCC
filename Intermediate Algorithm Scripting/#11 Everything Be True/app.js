b64da1f6d9829d6ff853a7d0e2d5f5f0de63f344
Dexter <vse.pisma.tut@gmail.com>
2018-06-18 22:09:11 +0300

    Everything Be True

---
+++ Intermediate Algorithm Scripting/#11 Everything Be True/app.js
@@ -0,0 +1,6 @@
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+ return collection.every(elem => elem[pre]);
+}
+
+document.write(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
